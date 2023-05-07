e5289af5e50123334524fd32351498c0ef4ca64e
KashmirLb <Kashmirlub@gmail.com>
2023-05-07 18:46:55 +0200

    Added checks in firebase app connection

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-import { initializeApp, cert} from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from 'firebase-admin/firestore'
 import { readFile } from 'fs/promises'
 
@@ -7,14 +7,29 @@ const createDbConnection = async db =>{
     const file = await readFile(`.firekey-${db}.json`)
     
     const serviceAccount = JSON.parse(file)
-    
-    initializeApp({
-      credential: cert(serviceAccount)
-    });
-    
-    const connection = getFirestore();
 
-    return connection
+    if(getApps?.length == 0){
+        initializeApp({
+          credential: cert(serviceAccount)
+        });
+
+        const connection = getFirestore();
+        return connection
+    }
+    else{
+        try{
+            let fireApp = initializeApp({
+                credential: cert(serviceAccount)
+              }, db);
+      
+              const connection = getFirestore(fireApp);
+              return connection
+
+        }
+        catch{
+            return null
+        }
+    }
 }
 
 export default createDbConnection
