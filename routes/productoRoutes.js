098a9a3f0387b24c1a23007658273aecee9cf0c0
KashmirLb <Kashmirlub@gmail.com>
2023-05-09 13:28:35 +0200

    printing origin

--- routes/productoRoutes.js
+++ routes/productoRoutes.js
@@ -11,6 +11,7 @@ router.get("/get-connection", (req, res) => {
 
     const whitelist = JSON.parse(process.env.WHITELIST_URL);
 
+    console.log(origin)
     console.log(whitelist)
     console.log(req.hostname)
     console.log(req.ip)
